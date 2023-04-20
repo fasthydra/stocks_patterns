c7236cf923dc00ad1f10b58fbbba509e0cb84ad3
alimaskin <alimaskin@gmail.com>
2023-04-20 17:07:28 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Searching for patterns on stock price charts',
+    author='stocks_patterns',
+    license='',
+)
