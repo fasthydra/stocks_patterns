650e059f36c18154e23747901370fe35aa511a73
Nikc-mk <3kaal6@gmail.com>
2023-05-01 18:38:44 +1000

    reformated

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Searching for patterns on stock price charts',
-    author='stocks_patterns_team',
-    license='',
+    version="0.1.0",
+    description="Searching for patterns on stock price charts",
+    author="stocks_patterns_team",
+    license="",
 )
