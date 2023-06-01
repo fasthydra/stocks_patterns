e3c67d1f52a80263006a4bf1f7b005a6f623268a
Nikc-mk <3kaal6@gmail.com>
2023-06-01 15:41:39 +1000

    создание образов docker pgadmin, pg, mlflow, настройка трекинга mlflow

---
+++ src/models/optimize.py
@@ -0,0 +1,119 @@
+import logging
+import time
+
+import numpy as np
+import optuna
+
+from src.models.clustering import get_clustering_model
+
+logger = logging.getLogger("file_logger")
+
+
+def optimize(
+    data: np.ndarray,
+    model_name: str,
+    model_prmt: dict,
+    opt_clusters_min_max_step: tuple,
+    best_cl: int = 10,
+    n_trials: int = 10,
+) -> dict:
+    """Подбирает параметры для датасета и модели кластеризации.
+
+    Args:
+         data (np.ndarray): Стандартизированный датасет.
+
+         all_clusters_min_max_step (tuple): Число кластеров, которые
+             подбирает модель (ОТ, ДО, ШАГ).
+
+         model_name (str): Имя модели для подбора параметров.
+
+         model_prmt (dict): Параметры для инициализации модели.
+            max_iter,
+            n_init,
+            n_clusters,
+
+         best_clusters (int): Число хороших кластеров, которые ищет модель.
+             Defaults to 10.
+
+         n_trials (int, optional): Количество проходов оптимизации.
+             Defaults to 10.
+             (если хочется пройти все варианты, то надо ставить хотя бы число
+             равное кол-ву сочетаний *4, но обычно хватает кол-ва сочетаний /2)
+
+     Returns:
+         best_params (dict): Подобранные параметры (общее число кластеров).
+
+    """
+
+    param_history = []
+    iteration = [1]  # в objective можно извне передать только list
+    seed = 0
+
+    logger.debug(f"Имя модели: {model_name}")
+    logger.debug(f"Параметры для модели: {model_prmt}")
+
+    def objective(trial):
+        try:
+            n_clusters_min = opt_clusters_min_max_step[0]
+            n_clusters_max = opt_clusters_min_max_step[1]
+            n_clusters_step = opt_clusters_min_max_step[2]
+
+            n_clusters = trial.suggest_int(
+                "n_clusters",
+                n_clusters_min,
+                n_clusters_max,
+                step=n_clusters_step,
+            )
+            logger.info(f"n_clusters = {n_clusters}:")
+
+            model_prmt["n_clusters"] = n_clusters
+
+            # test for repeated params
+            # we use tuple because that's easier to add more params this way
+            if (n_clusters) in param_history:
+                logger.info(
+                    f"Итерация {np.sum(iteration)} / {n_trials} завершена"
+                    + "в связи с повтором параметров"
+                )
+                iteration.append(1)
+                raise optuna.exceptions.TrialPruned()
+            param_history.append(n_clusters)
+        except Exception as ex:
+            logger.exception(
+                f"Ошибка при тесте повторяющихся параметров: {ex}"
+            )
+
+        np.random.seed(seed)
+        try:
+            ts = time.time()
+
+            model = get_clustering_model(model_name, model_prmt)
+            y_pred = model.fit_predict(data)
+            score = model.get_metric_std(data, y_pred, best_cl)
+
+            sec = time.time() - ts
+
+            logger.info(
+                f"Итерация {np.sum(iteration)} / {n_trials} завершена, "
+                + f"обработка {n_clusters} кластеров заняла "
+                + f"{np.round(sec/60,2)} минут"
+            )
+            iteration.append(1)
+
+            return score
+
+        except Exception as ex:
+            logger.debug(
+                f"Итерация {np.sum(iteration)} / {n_trials} завершена"
+                + " из-за слишком большого числа заданных кластеров"
+            )
+            iteration.append(1)
+            logger.exception(ex)
+            raise optuna.exceptions.TrialPruned()
+
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective, n_trials=2, show_progress_bar=False)
+
+    logger.info(f"Лучшие параметры: {study.best_params}")
+
+    return study.best_params
---
+++ workflows/optuna_optimize.py
@@ -0,0 +1,109 @@
+import logging
+import logging.config
+
+import click
+import dotenv
+import mlflow
+import numpy as np
+import yaml
+
+from src.logger.log_settings import LOGGING_CONFIG
+from src.models.optimize import optimize
+
+dotenv.load_dotenv()
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("test_dvc")
+
+
+@click.command()
+@click.option(
+    "--in_file",
+    required=True,
+    help="Input file full path to read the trading data",
+)
+@click.option("--model_name", required=True, type=str, help="Имя модели")
+@click.option("--max_iter", required=True, type=int, help="???")
+@click.option("--n_init", required=True, type=int, help="???")
+@click.option("--n_clusters", required=True, type=int, help="???")
+@click.option(
+    "--opt_min",
+    required=True,
+    type=int,
+    help="Минимальное число кластеров для подбора",
+)
+@click.option(
+    "--opt_max",
+    required=True,
+    type=int,
+    help="Максимальное число кластеров для подбора",
+)
+@click.option(
+    "--opt_step",
+    required=True,
+    type=int,
+    help="Шаг для подбора количества кластеров",
+)
+@click.option(
+    "--best_clusters",
+    required=True,
+    type=int,
+    help="Число хороших кластеров, которые ищет модель.",
+)
+@click.option("--n_trials", type=int, help="Количество проходов оптимизации")
+@click.option("--par_file", type=str, help="Путь для сохранения параметров")
+def main(
+    in_file: str,
+    model_name: str,
+    max_iter: int,
+    n_init: int,
+    n_clusters: int,
+    opt_min: int,
+    opt_max: int,
+    opt_step: int,
+    best_clusters: int,
+    n_trials: int,
+    par_file: str,
+):
+    logging.config.dictConfig(LOGGING_CONFIG)
+    logger = logging.getLogger("file_logger")
+
+    logger.debug("Начало подбора параметров.")
+
+    try:
+        input_df = np.load(in_file)
+    except Exception as ex:
+        logger.exception(f"Ошибка чтения файла: {ex}")
+
+    try:
+        best_params = optimize(
+            data=input_df,
+            model_name=model_name,
+            model_prmt={
+                "max_iter": max_iter,
+                "n_init": n_init,
+                "n_clusters": n_clusters,
+            },
+            opt_clusters_min_max_step=(opt_min, opt_max, opt_step),
+            best_cl=best_clusters,
+            n_trials=n_trials,
+        )
+        logger.info("Параметры модели подобраны.")
+        logger.info(best_params)
+    except Exception as ex:
+        logger.exception(f"Ошибка при подборе параметров: {ex}")
+
+    try:
+        with open(par_file, "w") as f:
+            yaml.dump(
+                best_params, f, sort_keys=False, default_flow_style=False
+            )
+
+    except Exception as ex:
+        logger.exception(f"Ошибка при сохранении параметров: {ex}")
+
+    with mlflow.start_run():
+        mlflow.log_param("my", best_params)
+
+
+if __name__ == "__main__":
+    main()
