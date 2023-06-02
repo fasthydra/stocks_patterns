8d49d8ba3b030e739735e26d577d9d689b2aafdb
fasthydra <36157436+fasthydra@users.noreply.github.com>
2023-06-02 15:20:48 +0300

    Mlflow (#12)
    * создание образов docker pgadmin, pg, mlflow, настройка трекинга mlflow
    
    * dvc + mlflow
    
    ---------
    
    Co-authored-by: Nikc-mk <3kaal6@gmail.com>

--- workflows/load_moex_stocks_candles.py
+++ workflows/load_moex_stocks_candles.py
@@ -3,9 +3,16 @@ import logging.config
 import pprint
 
 import dvc.api
+import mlflow
+from dotenv import load_dotenv
 
 from src.data.load_from_moex import load_stocks
 from src.logger.log_settings import LOGGING_CONFIG
+
+load_dotenv()
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("test_dvc_new")
 
 params = dvc.api.params_show()["load_moex"]
 
@@ -13,6 +20,8 @@ logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger("file_logger")
 
 logger.debug("Параметры запуска: \n" + pprint.pformat(params, compact=True))
+
+mlflow.log_params(params)
 
 df = load_stocks(
     security=params["security"],
@@ -22,13 +31,14 @@ df = load_stocks(
 )
 
 try:
-    df.to_csv(sep=",", path_or_buf=params["paths"]["output"])
+    file_to_save = params["paths"]["output"]
+    df.to_csv(sep=",", path_or_buf=file_to_save)
     logger.info(
         f"Данные (строк: {df.shape[0]}) успешно загружены /"
-        f'в файл {params["paths"]["output"]}'
+        f"в файл {file_to_save}"
     )
+    mlflow.log_artifact(file_to_save)
 except Exception as ex:
     logger.exception(
-        f'Ошибка при сохранении датасета в файл {params["paths"]["output"]}: /'
-        f" {ex}"
+        f"Ошибка при сохранении датасета в файл {file_to_save}: /" f" {ex}"
     )
